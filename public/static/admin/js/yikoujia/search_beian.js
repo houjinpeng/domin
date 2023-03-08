8bd13169856f89f685b111faaa91a7438bef7eaf
houjinpeng <aihoujinpeng@163.com>
2023-03-08 09:31:35 +0800

    seo备案

--- public/static/admin/js/yikoujia/search_beian.js
+++ public/static/admin/js/yikoujia/search_beian.js
@@ -71,7 +71,7 @@ define(["jquery", "easy-admin"], function ($, ea) {
                                 resp.data.forEach(function (item) {
                                     let d = false
                                     try{
-                                        d =  item['data']['list'][0]
+                                        d =  item['params']['list'][0]
                                     }catch (e) {
 
                                     }
