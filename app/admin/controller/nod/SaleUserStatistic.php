e02fc158a452762e28c5f1cf160a619e6fe2b4b7
houjinpeng <aihoujinpeng@163.com>
2023-05-08 13:27:33 +0800

    xx

--- app/admin/controller/nod/SaleUserStatistic.php
+++ app/admin/controller/nod/SaleUserStatistic.php
@@ -48,21 +48,27 @@ class SaleUserStatistic extends AdminController
         $profit_price_list = [];
         $sale_count_list = [];
         $sale_user_list = [];
+        $t = [date('y-m') .'-01 00:00:00',date('y-m') .'-31 23:59:59'];
+//        $t = ['2021-01-01 00:00:00',date('y-m') .'-31 23:59:59'];
         foreach ($all_sale_user as $user){
             $sale_user_list[] = $user['username'];
             //获取销售员的销售条数及利润
-            $count = $this->account_info_model->where('sale_user_id','=',$user['id'])->where('type','=','3')->group('good_name')->count();//销售单
-            $th_count = $this->account_info_model->where('sale_user_id','=',$user['id'])->where('type','=','6')->count();//退货单销售单
+            $count = $this->account_info_model
+                ->where('operate_time','BETWEEN',[$t[0],$t[1]])
+                ->where('sale_user_id','=',$user['id'])->where('type','=','3')->group('good_name')->count();//销售单
+            $th_count = $this->account_info_model
+                ->where('operate_time','BETWEEN',[$t[0],$t[1]])
+                ->where('sale_user_id','=',$user['id'])->where('type','=','6')->count();//退货单销售单
             $sale_count_list[] = $count-$th_count;
 
             $cate = $this->category_model->where('name','=','销售费用')->find();
             if (!empty($cate)){
 
                 //计算利润
-                $profit_price = $this->account_info_model->where('sale_user_id','=',$user['id'])->whereRaw('(type=3 or type=6 or type=9  or type=8 or category_id='.$cate['id'].')')->sum('profit_price');//销售单
+                $profit_price = $this->account_info_model   ->where('operate_time','BETWEEN',[$t[0],$t[1]])->where('sale_user_id','=',$user['id'])->whereRaw('(type=3 or type=6 or type=9  or type=8 or category_id='.$cate['id'].')')->sum('profit_price');//销售单
             }else{
                 //计算利润
-                $profit_price = $this->account_info_model->where('sale_user_id','=',$user['id'])->whereRaw('(type=3 or type=6 or type=9  or type=8)')->sum('profit_price');//销售单
+                $profit_price = $this->account_info_model   ->where('operate_time','BETWEEN',[$t[0],$t[1]])->where('sale_user_id','=',$user['id'])->whereRaw('(type=3 or type=6 or type=9  or type=8)')->sum('profit_price');//销售单
             }
 
 
--- public/static/admin/js/nod/sale_user_statistic.js
+++ public/static/admin/js/nod/sale_user_statistic.js
@@ -1,4 +1,4 @@
-define(["jquery", "easy-admin","echarts"], function ($, ea,echarts) {
+define(["jquery", "easy-admin", "echarts"], function ($, ea, echarts) {
 
     var init = {
         table_elem: '#currentTable',
@@ -19,7 +19,7 @@ define(["jquery", "easy-admin","echarts"], function ($, ea,echarts) {
             var sale_count_list = JSON.parse($('#sale_count_list').val())
             var profit_price_list = JSON.parse($('#profit_price_list').val())
 
-            function set_chart(user_list,sale_count_list,profit_price_list) {
+            function set_chart(user_list, sale_count_list, profit_price_list) {
                 return {
                     tooltip: {
                         trigger: 'axis',
@@ -95,36 +95,37 @@ define(["jquery", "easy-admin","echarts"], function ($, ea,echarts) {
                 }
             }
 
+            var date = new Date();
+            var year = date.getFullYear();
+            var month = date.getMonth() + 1;
 
 
             laydate.render({
                 elem: '#test1' //指定元素
-                ,max: 1 //7天后
-                ,type: 'datetime'
-                ,range: true
+                , max: 1 //7天后
+                , type: 'datetime'
+                , range: true
+                , value: year + '-' + month + '-01 00:00:00 - ' + ea.GetDateStr(0) + ' 23:59:59'
             });
 
 
-            myChart.setOption(set_chart(user_list,sale_count_list,profit_price_list));
+            myChart.setOption(set_chart(user_list, sale_count_list, profit_price_list));
 
             $('#search').click(function () {
                 //获取时间
                 let t = $('#test1').val()
-                if (t ===''){
-                    layer.msg('时间范围不能为空~',{icon:2})
+                if (t === '') {
+                    layer.msg('时间范围不能为空~', {icon: 2})
                     return false
                 }
                 ea.request.get({
-                    url:'get_sale_user_profit?time='+t,
-                },function (resp) {
-                    myChart.setOption(set_chart(resp.data.sale_user_list,resp.data.sale_count_list,resp.data.profit_price_list));
+                    url: 'get_sale_user_profit?time=' + t,
+                }, function (resp) {
+                    myChart.setOption(set_chart(resp.data.sale_user_list, resp.data.sale_count_list, resp.data.profit_price_list));
                 })
 
 
-
-
             })
-
 
 
             ea.listen();
